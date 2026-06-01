09cc9c8238f1828c97cefe78c6c8af1caa1d64d0
diaoyugan <67471429+diaoyugan@users.noreply.github.com>
2026-06-01 17:03:52 +0800

    port to 26.2-pre.2

--- fabric/src/main/java/top/diaoyugan/enchanted_ui/client/configScreen/ModMenuIntegration.java
+++ fabric/src/main/java/top/diaoyugan/enchanted_ui/client/configScreen/ModMenuIntegration.java
@@ -1,14 +1,14 @@
-//package top.diaoyugan.enchanted_ui.client.configScreen;
-//
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import top.diaoyugan.enchanted_ui.client.gui.screen.DemoScreen;
-//
-//
-//public class ModMenuIntegration implements ModMenuApi {
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return DemoScreen::new;
-//    }
-//}
+package top.diaoyugan.enchanted_ui.client.configScreen;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import top.diaoyugan.enchanted_ui.client.gui.screen.DemoScreen;
+
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return DemoScreen::new;
+    }
+}
 
